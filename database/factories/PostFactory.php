95a9f813df158bd80ec92da18f69d816c144d998
funayamateppei <t.funayama0217@gmail.com>
2023-02-25 11:40:54 +0900

    Factoryでのダミーデータ作成

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -17,7 +17,8 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'title' => fake()->colorName(),
+            'content' => fake()->realText(150),
         ];
     }
 }
