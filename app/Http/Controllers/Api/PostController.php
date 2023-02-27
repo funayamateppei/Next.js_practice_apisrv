9102ad4ce2cb550d121e787c2d40a8a1b42bd708
funayamateppei <t.funayama0217@gmail.com>
2023-02-27 17:43:29 +0900

    APIが動くかどうか確認するための関数作成(PostController)

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 use App\Models\Post;
 
@@ -16,7 +17,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        return '1';
+        $user = Auth::user();
+        return $user;
     }
 
     public function show(Request $request)
